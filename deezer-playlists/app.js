08ab6330bbb3c21a30ce774346ba5c12a9b0f620
Jazmindt29 <jazmin.diaz4616@alumnos.udg.mx>
2026-04-09 18:59:57 -0600

    ya se agrego la demostracion de canciones

--- deezer-playlists/app.js
+++ deezer-playlists/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
@@ -14,6 +15,29 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index", { tracks: [], artist: "" });
+});
+
+app.post("/search", async (req, res) => {
+  const artist = req.body.artist;
+
+  try {
+    const response = await axios.get("https://api.deezer.com/search", {
+      params: {
+        q: `artist:"${artist}"`,
+        limit: 10
+      }
+    });
+
+    const tracks = response.data.data || [];
+    res.render("index", { tracks, artist });
+  } catch (error) {
+    console.error("Error con Deezer:", error.message);
+    res.render("index", { tracks: [], artist });
+  }
+});
+
+app.get("/playlists", (req, res) => {
+  res.render("playlists");
 });
 
 app.listen(PORT, () => {
